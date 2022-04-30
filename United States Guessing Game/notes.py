f03881525d653c64f1b9ddeca595fd2f165938e7
RegalCodes <104613081+RegalCodes@users.noreply.github.com>
2022-04-29 22:21:23 -0700

    Add files via upload

---
+++ United States Guessing Game/notes.py
@@ -0,0 +1,10 @@
+#Notes
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+turtle.onscreenclick(get_mouse_click_coor)   # This gets coordinates on screen click
+
+
+
+turtle.mainloop() #--> Keeps the window open
